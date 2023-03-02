65ce8a58c23497bd9e772fc341ed05b98ce99edd
AndrewKostenko <kostenkoandrew01@gmail.com>
2023-03-02 08:05:14 +0200

    12. grid

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/2/21/ZDF_logo%21_Logo_2021.svg" alt="" />
+		</header>
+	)
+}
+
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src="https://mywowo.net/media/images/cache/dubai_img_worlds_of_adventure_01_presentazione_jpg_1200_630_cover_85.jpg" alt="" />
+			</div>
+			<div>ava</div>
+			<div>my
+				<div>
+					new
+					<div>
+						<div>pos1</div>
+					</div>
+					<div>
+						<div>post1</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+
+export default Profile;
